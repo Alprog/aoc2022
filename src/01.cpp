c59ea04ec29575687a65593b0816fd6856515cc5
Alexander Tuzhik <alprog@hotmail.com>
2022-12-01 14:19:45 +0100

    01 solution part I

--- src/01.cpp
+++ src/01.cpp
@@ -3,5 +3,22 @@
 
 void puzzle<1>::run(std::vector<std::string>& lines)
 {
+	lines.push_back("");
 
+	int max = 0;
+	int cur = 0;
+	for (auto& line : lines)
+	{
+		if (!line.empty())
+		{
+			cur += atoi(line.c_str());
+		}
+		else
+		{
+			max = std::max(cur, max);
+			cur = 0;
+		}
+	}
+
+	std::cout << max;
 }
