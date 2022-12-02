abfa9a6992456cc642ec81f76967389c3d8a6b30
Alexander Tuzhik <alprog@hotmail.com>
2022-12-02 06:30:05 +0100

    03 boilerplate

---
+++ src/03.cpp
@@ -0,0 +1,12 @@
+
+#include "puzzles.h"
+
+void puzzle<3, 1>::run(std::vector<std::string>& lines)
+{
+
+}
+
+void puzzle<3, 2>::run(std::vector<std::string>& lines)
+{
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,10 @@
 #include <format>
 #include <type_traits>
 
-constexpr int puzzle_count = 2;
+constexpr int puzzle_count = 3;
 constexpr int part_count = 2;
 
-constexpr int active_puzzle_index = 2;
+constexpr int active_puzzle_index = 3;
 
 template <int>
 struct day_index {};
