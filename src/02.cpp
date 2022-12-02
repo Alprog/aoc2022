94027f93d5857b453a9cb961e21882b5bdd8788c
Alexander Tuzhik <alprog@hotmail.com>
2022-12-02 06:26:03 +0100

    02

--- src/02.cpp
+++ src/02.cpp
@@ -3,10 +3,32 @@
 
 void puzzle<2, 1>::run(std::vector<std::string>& lines)
 {
+	auto score = 0;
+	for (auto& line : lines)
+	{
+		auto enemy = line[0] - 'A';
+		auto my = line[2] - 'X';
 
+		auto result = (my - enemy + 4) % 3;
+
+		score += (my + 1) + (result * 3);
+
+	}
+	std::cout << score << "\n";
 }
 
 void puzzle<2, 2>::run(std::vector<std::string>& lines)
 {
+	auto score = 0;
+	for (auto& line : lines)
+	{
+		auto enemy = line[0] - 'A';
+		auto result = line[2] - 'X';
 
+		auto my = (result + enemy + 2) % 3;
+
+		score += (my + 1) + (result * 3);
+
+	}
+	std::cout << score << "\n";
 }
