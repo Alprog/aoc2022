815c34a99a24aaa5a3c49cf05e07f54c1639004d
Alexander Tuzhik <alprog@hotmail.com>
2022-12-03 06:25:52 +0100

    initial

--- src/03.cpp
+++ src/03.cpp
@@ -2,10 +2,73 @@
 
 void puzzle<3, 1>::run(std::vector<std::string>& lines)
 {
+	auto total = 0;
 
+	for (auto& line : lines)
+	{
+		auto size = line.size();
+		auto half_size = size / 2;
+
+		for (int i = 0; i < half_size; i++)
+		{
+			char c = line[i];
+
+			if (std::find(line.begin() + half_size, line.end(), c) != line.end())
+			{
+				if (c >= 'A' && c <= 'Z')
+				{
+					total += c - 'A' + 27;
+				}
+				else
+				{
+					total += c - 'a' + 1;
+				}
+				
+				break;
+			}
+			
+		}
+
+	}
+
+	std::cout << total << "\n";
 }
 
 void puzzle<3, 2>::run(std::vector<std::string>& lines)
 {
+	auto total = 0;
 
+	for (int j = 0; j < lines.size(); j += 3)
+	{
+		auto line = lines[j];
+		auto size = line.size();
+
+		for (int i = 0; i < size; i++)
+		{
+			char c = line[i];
+
+			auto lineA = lines[j + 1];
+			auto lineB = lines[j + 2];
+
+			if (std::find(lineA.begin(), lineA.end(), c) != lineA.end())
+			{
+				if (std::find(lineB.begin(), lineB.end(), c) != lineB.end())
+				{
+					if (c >= 'A' && c <= 'Z')
+					{
+						total += c - 'A' + 27;
+					}
+					else
+					{
+						total += c - 'a' + 1;
+					}
+
+					break;
+				}
+			}
+
+		}
+	}
+
+	std::cout << total << "\n";
 }
