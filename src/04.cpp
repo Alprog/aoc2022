59dd807f680f80f02f18d29c211c2c566719c1f6
Alexander Tuzhik <alprog@hotmail.com>
2022-12-04 06:19:59 +0100

    puzzle 4

--- src/04.cpp
+++ src/04.cpp
@@ -4,10 +4,54 @@
 
 void puzzle<4, 1>::run(std::vector<std::string>& lines)
 {
+	auto total = 0;
 
+	const int arg_count = 4;
+
+	int arg[arg_count];
+	for (auto& line : lines)
+	{
+		str_utils::replace_all(line, ",", "-");
+		auto arr = str_utils::split(line, "-");
+		for (int i = 0; i < arg_count; i++)
+		{
+			arg[i] = std::atoi(arr[i].c_str());
+		}
+
+		auto a = arg[0] >= arg[2] && arg[1] <= arg[3];
+		auto b = arg[0] <= arg[2] && arg[1] >= arg[3];
+		if (a || b)
+		{
+			total++;
+		}
+	}
+
+	std::cout << total << "\n";
 }
 
 void puzzle<4, 2>::run(std::vector<std::string>& lines)
 {
-	
+	auto total = 0;
+
+	const int arg_count = 4;
+
+	int arg[arg_count];
+	for (auto& line : lines)
+	{
+		str_utils::replace_all(line, ",", "-");
+		auto arr = str_utils::split(line, "-");
+		for (int i = 0; i < arg_count; i++)
+		{
+			arg[i] = std::atoi(arr[i].c_str());
+		}
+
+		auto a = arg[0] >= arg[3] && arg[1] <= arg[2];
+		auto b = arg[0] <= arg[3] && arg[1] >= arg[2];
+		if (a || b)
+		{
+			total++;
+		}
+	}
+
+	std::cout << total << "\n";
 }
