48ac2a5279d21eab1548ed7b5d8a9d4198b8fe34
Alexander Tuzhik <alprog@hotmail.com>
2022-12-08 07:26:38 +0100

    boilerplate 9

--- src/09.cpp
+++ src/09.cpp
@@ -3,14 +3,14 @@
 #include "string_utils.h"
 #include <functional>
 
-//puzzle<9, 1> X = [](input& input) -> output
-//{
-//	int total = 0;
-//
-//	for (auto& line : input.lines)
-//	{
-//
-//	}
-//
-//	return total;
-//};
+puzzle<9, 1> X = [](input& input) -> output
+{
+	int total = 0;
+
+	for (auto& line : input.lines)
+	{
+
+	}
+
+	return total;
+};
