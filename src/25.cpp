dd198559d911caae9658c8bf2f01080a2f23a92e
Alexander Tuzhik <alprog@hotmail.com>
2022-12-14 08:27:07 +0100

    boilerplate 15-24

---
+++ src/25.cpp
@@ -0,0 +1,14 @@
+#include "puzzle_handlers.h"
+
+#include "string_utils.h"
+#include <functional>
+
+//puzzle<25, 1> X = [](input& input) -> output
+//{
+//	for (auto& line : input.lines)
+//	{
+//
+//	}
+//
+//	return 0;
+//};
